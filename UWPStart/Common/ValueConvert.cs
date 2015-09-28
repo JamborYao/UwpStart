eb510ac9c293c9c6ba328b9e847550354d7368b5
Jambor <jamboryao@hotmail.com>
2015-09-28 23:40:04 +0800

    add map and value converter

---
+++ UWPStart/Common/ValueConvert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWPStart.Common
+{
+    public class ValueConvert:IValueConverter
+    {
+        public System.Object Convert(System.Object value, Type targetType, System.Object parameter, System.String language)
+        {
+            return (int)((int)value+100);
+        }
+        public System.Object ConvertBack(System.Object value, Type targetType, System.Object parameter, System.String language)
+        {
+            return new object();
+        }
+    }
+}
---
+++ UWPStart/Pages/BingMap.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPStart.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BingMap : Page
+    {
+        public BingMap()
+        {
+            this.InitializeComponent();
+            MapControl1.MapServiceToken = "0qbroQWMYoqQi5WB16Dv~8Sn6iF5oMLL9QCVTlZ1sxQ~AjOib3htUA3nFNMXDaGjnFoLjBLPkRjOmOf5RT-n9rsOjd7IHjm_xUO2ARM4Kwki";
+            Get();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+            base.OnNavigatedTo(e);
+        }
+        public async void  Get()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+
+                    // Get the current location
+                    Geolocator geolocator = new Geolocator();
+                    Geoposition pos = await geolocator.GetGeopositionAsync();
+                    Geopoint myLocation = pos.Coordinate.Point;
+
+                    // Set map location
+                    MapControl1.Center = myLocation;
+                    MapControl1.ZoomLevel = 12;
+                    MapControl1.LandmarksVisible = true;
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+                    // Handle when access to location is denied
+                    break;
+
+                case GeolocationAccessStatus.Unspecified:
+                    // Handle when an unspecified error occurs
+                    break;
+            }
+        }
+    }
+}
