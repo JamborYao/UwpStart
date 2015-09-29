3ab9ab2f5a1880f0b3e2901a0c97beff767d2840
Jambor <jamboryao@hotmail.com>
2015-09-29 22:48:02 +0800

    add visual manager

--- UWPStart/Common/HttpHelper.cs
+++ UWPStart/Common/HttpHelper.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Web.Http;
 
 namespace UWPStart.Common
 {
@@ -28,6 +30,17 @@ namespace UWPStart.Common
             return content;
            // httpContent.Text = content;
             //outPutView.NavigateToString(content);
+        }
+
+        public  static async Task<string> HttpClientGetThreads()
+        {
+            string url = "http://10.168.172.243:8080/ThreadDetailManagerService.svc/getThreadsByFilter2?startTime=08/30/2015&endTime=09/29/2015&owner=none&status=-1";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage httpResponse= await client.GetAsync(new Uri(url));
+            httpResponse.EnsureSuccessStatusCode();
+            string httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+            return httpResponseBody;
+
         }
     }
 }
--- UWPStart/Pages/RelatedDesign.xaml.cs
+++ UWPStart/Pages/RelatedDesign.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -26,40 +27,31 @@ namespace UWPStart.Pages
         {
             this.InitializeComponent();
             this.left.ItemsSource = Enumerable.Range(1, 10).ToList();
+         
         }
+       
         private void full_back_Click(object sender, RoutedEventArgs e)
         {
-            // 对于我们的页面来说，full_back按钮是应该隐藏的，因为没有上一层页面
-            // 所以这里我们忽略掉，但是按钮还是留着，你可以自己来做个隐藏的逻辑
+          
         }
 
         private void detail_back_Click(object sender, RoutedEventArgs e)
         {
-            // 这里就是为什么我们在VisualState里重新设置属性的原因
-
-            // 显示左侧的list
-            // 隐藏右侧内容
+           
             this.left.Visibility = Visibility.Visible;
             this.right.Visibility = Visibility.Collapsed;
-
-            // 隐藏副标题
-            // 显示主标题
+            
             this.fullPanel.Visibility = Visibility.Visible;
             this.detailPanel.Visibility = Visibility.Collapsed;
         }
 
         private void left_ItemClick(object sender, ItemClickEventArgs e)
         {
-            // 这里我们需要判断下，是否需要切换隐藏
             if (this.ActualWidth <= 500)
             {
-                // 显示左侧的list
-                // 隐藏右侧内容
                 this.left.Visibility = Visibility.Collapsed;
                 this.right.Visibility = Visibility.Visible;
-
-                // 隐藏副标题
-                // 显示主标题
+                
                 this.fullPanel.Visibility = Visibility.Collapsed;
                 this.detailPanel.Visibility = Visibility.Visible;
             }
